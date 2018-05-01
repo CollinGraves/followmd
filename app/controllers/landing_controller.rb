c4b0ebd243b0e464c1494212554bf052af54dd9c
Collin Graves <collinrgraves@gmail.com>
2018-05-01 18:34:26 -0500

    lots of customization stuff

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,5 @@
+class LandingController < ApplicationController
+  layout '_base'
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,34 @@
+class SubdomainConstraint
+  def self.matches?(request)
+    subdomains = %w( www admin followmd app application)
+    request.subdomain.present? && !subdomains.include?(request.subdomain)
+  end
+end
+
+class MaindomainConstraint
+  def self.matches?(request)
+    subdomains = %w( www admin followmd app application)
+    !request.subdomain.present? || subdomains.include?(request.subdomain)
+  end
+end
+
 Rails.application.routes.draw do
-  resources :organizations
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  constraints SubdomainConstraint do
+    require 'sidekiq/web'
+    mount Sidekiq::Web => '/sidekiq'
+
+    root 'home#dashboard'
+  end
+
+  constraints MaindomainConstraint do
+    root 'landing#index'
+
+    get 'about', to: 'landing#about'
+    get 'pricing', to: 'pricing#index'
+    get 'tos', to: 'legal#tos'
+    get 'privacy', to: 'legal#privacy'
+  end
 end
